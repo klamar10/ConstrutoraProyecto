84b9fb6abb1aef5dad038c0ced58b6c7f10734d7
ADRIAN <lm_adrian_98@hotmail.com>
2019-06-05 22:39:11 -0500

    commit hoy

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'static/about'
+
+  get 'static/services'
+
+  #get 'home/index'
+  root 'about/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
