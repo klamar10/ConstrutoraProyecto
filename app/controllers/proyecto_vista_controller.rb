f01d750d9beab9cf98e7f13080c748a20dbfc9e3
Yuliza <torreshinostrozay@gmail.com>
2019-06-20 23:01:50 -0500

    Update proyecto_vista_controller.rb
    Avances de vista de proyectos

--- app/controllers/proyecto_vista_controller.rb
+++ app/controllers/proyecto_vista_controller.rb
@@ -1,26 +1,54 @@
 class ProyectoVistaController < ApplicationController
 
-  def caracteristica
-  end
-  
-def ubicacion
-end
-def caracteristica
-end
+	def proyecto
+		# Aca debe usarse Post.all
+		# Declarando Data ficticia
+		@listProyecto = [
+			[ id = 7, name = 'Miraflores', imagen = '/assets/Proy3.jpg', metros = '120mts', areaComunes = 'No', cantBaños = 5,
+			  planos = [
+			  	[ imagen= 'hola.jpg', descripcion= 'mejorandola'],
+			  	[ imagen= 'hola.jpg', descripcion= 'mejorandola'],
+			  	[ imagen= 'hola.jpg', descripcion= 'mejorandola']
+			  ] 
+			],
+			[ id = 4, name = 'Barranco', imagen = '/assets/Proy2.jpg' ],
+			[ id = 5, name = 'Magdalena', imagen = '/assets/Proy3.jpg' ]
+		]
 
-def departamento
-end
+		# Una tabla de proyecto con datos basicos ----- asigna un ID, Ejemplo 5, id=5, name=Miraflores, direcion=aaaa, imagen=''
+		# Una tabla llamada caracteristicas y la relacionan con el id del PRoyecto 
+		# Ejemplo : Id=5 , metros=10, sshh=5, estacionamiento=si
 
-def plano
-end
-def PBarranco
-end
-def PMiraflores
-end
-def PSurco
-end
+	end
 
-def consultar
-end
+	def detalle
+		# Aca debe usarse Post.find(params[:id])
+		@listProyecto = [
+			[ id = 7, name = 'Miraflores', imagen = '/assets/Proy3.jpg', metros = '120mts', areaComunes = 'No', cantBaños = 5,
+			  planos = [
+			  	[ imagen= 'hola.jpg', descripcion= 'mejorandola'],
+			  	[ imagen= 'hola.jpg', descripcion= 'mejorandola'],
+			  	[ imagen= 'hola.jpg', descripcion= 'mejorandola']
+			  ] 
+			],
+			[ id = 4, name = 'Barranco', imagen = '/assets/Proy2.jpg' ],
+			[ id = 5, name = 'Magdalena', imagen = '/assets/Proy3.jpg' ]
+		]
+
+		@listProyecto.each do |proyecto|
+
+			if proyecto[0].to_i  == params[:id].to_i 
+				@proyectoSearch = proyecto
+			end
+		end
+
+		# Fin ficticio
+	end
+
+	def consultar
+	end
+
+	def ubicacion
+	end
 	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,21 +2,21 @@ Rails.application.routes.draw do
 root :to => 'menu#inicio'	
   get 'menu/inicio'
   get 'menu/Nosotros'
-  get 'proyecto_vista/proyecto'  
-  get 'proyecto_vista/ubicacion'
-  get 'proyecto_vista/PBarranco'
-  get 'proyecto_vista/PMiraflores'
-  get 'proyecto_vista/PSurco'
-   post 'pages/mimetodo'
-  get 'proyecto_vista/caracteristica'
-  get 'proyecto_vista/departamento'
-  get 'proyecto_vista/plano'
-  get 'proyecto_vista/consultar'
+  post 'pages/mimetodo'
   resources :posts
+
   #welcom / registrar
   get 'welcome/index'
   get 'welcome/RegProy'
   get 'welcome/Servicio'
  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Routes Yuliza
+  get '/proyecto', to: 'proyecto_vista#proyecto'
+  get '/proyecto/:id', to: 'proyecto_vista#detalle'
+  get '/proyecto_vista/consultar'
+  get '/ubicacion',  to: 'proyecto_vista#ubicacion'
+
+
 end
