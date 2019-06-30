6d02493da4944f796f6e6b116761a2cb88f221c1
Yuliza <torreshinostrozay@gmail.com>
2019-06-30 00:56:47 -0500

    Create cancha1.jpg

--- app/controllers/proyecto_vista_controller.rb
+++ app/controllers/proyecto_vista_controller.rb
@@ -4,16 +4,29 @@ class ProyectoVistaController < ApplicationController
 		# Aca debe usarse Post.all
 		# Declarando Data ficticia
 		@listProyecto = [
-			[ id = 7, name = 'Miraflores', imagen = '/assets/Proy3.jpg', metros = '120mts', areaComunes = 'No', cantBaños = 5,
+			[ id = 7, name = 'Miraflores', imagen = '/assets/Proy1.jpg', metros = '120mts', areaComunes = 'No', cantBaños = 5,
 			  planos = [
-			  	[ imagen= 'hola.jpg', descripcion= 'mejorandola'],
-			  	[ imagen= 'hola.jpg', descripcion= 'mejorandola'],
+			  	[ imagen= 'park1.jpg', descripcion= 'Parque'],
 			  	[ imagen= 'hola.jpg', descripcion= 'mejorandola']
 			  ] 
 			],
-			[ id = 4, name = 'Barranco', imagen = '/assets/Proy2.jpg' ],
-			[ id = 5, name = 'Magdalena', imagen = '/assets/Proy3.jpg' ]
+			[ id = 4, name = 'Barranco', imagen = '/assets/Proy2.jpg', metros = '500mts', areaComunes = 'Si', cantBaños = 6, 
+				planos = [
+					[ descripcion= 'Areas Verdes'],
+			  		[ descripcion= 'mejorandola'],
+			  		[ descripcion= 'mejorandola']	
+				]
+			],
+			[ id = 5, name = 'Magdalena', imagen = '/assets/Proy3.jpg' , metros = '500mts', areaComunes = 'Si', cantBaños = 6, 
+				planos = [
+					[ descripcion= 'Areas Verdes'],
+			  		[ descripcion= 'mejorandola'],
+			  		[ descripcion= 'mejorandola']	
+				]
+			]
 		]
+
+
 
 		# Una tabla de proyecto con datos basicos ----- asigna un ID, Ejemplo 5, id=5, name=Miraflores, direcion=aaaa, imagen=''
 		# Una tabla llamada caracteristicas y la relacionan con el id del PRoyecto 
@@ -24,16 +37,28 @@ class ProyectoVistaController < ApplicationController
 	def detalle
 		# Aca debe usarse Post.find(params[:id])
 		@listProyecto = [
-			[ id = 7, name = 'Miraflores', imagen = '/assets/Proy3.jpg', metros = '120mts', areaComunes = 'No', cantBaños = 5,
+			[ id = 7, name = 'Miraflores', imagen = '/assets/Proy1.jpg', metros = '120mts', areaComunes = 'No', cantBaños = 5,
 			  planos = [
-			  	[ imagen= 'hola.jpg', descripcion= 'mejorandola'],
-			  	[ imagen= 'hola.jpg', descripcion= 'mejorandola'],
+			  	[ descripcion= 'Parque'],
 			  	[ imagen= 'hola.jpg', descripcion= 'mejorandola']
 			  ] 
 			],
-			[ id = 4, name = 'Barranco', imagen = '/assets/Proy2.jpg' ], v
-			[ id = 5, name = 'Magdalena', imagen = '/assets/Proy3.jpg' ]
+		[ id = 4, name = 'Barranco', imagen = '/assets/Proy2.jpg', metros = '500mts', areaComunes = 'Si', cantBaños = 6, 
+				planos = [
+					[ descripcion= 'Areas Verdes'],
+			  		[ descripcion= 'mejorandola'],
+			  		[ descripcion= 'mejorandola']	
+				]
+			],			
+		[ id = 5, name = 'Magdalena', imagen = '/assets/Proy3.jpg', metros = '500mts', areaComunes = 'Si', cantBaños = 6, 
+				planos = [
+					[ descripcion= 'Areas Verdes'],
+			  		[ descripcion= 'mejorandola'],
+			  		[ descripcion= 'mejorandola']	
+				]
+			]
 		]
+
 
 		@listProyecto.each do |proyecto|
 
